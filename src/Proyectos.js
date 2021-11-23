5ac1b51bb0d35c211aefab32a96d981e1773b1a4
guadi-torino <guadi.torino@gmail.com>
2021-11-23 19:15:54 -0300

    2 files modified

--- src/Proyectos.js
+++ src/Proyectos.js
@@ -10,25 +10,35 @@ const Proyectos = () => {
         <h1>Proyectos</h1>
         <div className="card2">
           <div className="header">
-            Dummy API
+            <p>Dummy API</p>
             <img
               className="imagen"
               src={
                 'https://stackblitz.com/files/react-phgqov/github/guadi-torino/TP10/master/dummyapi.PNG'
               }
             />
+            <div className="btn">
+              <Link to="/proyectos">
+                <p style={{ textDecoration: 'none' }}>Ver proyecto</p>
+              </Link>
+            </div>
           </div>
         </div>
         <br />
         <div className="card2">
           <div className="header">
-            Administrador de pacientes{' '}
+            <p> Administrador de pacientes</p>
             <img
               className="imagen"
               src={
                 'https://stackblitz.com/files/react-phgqov/github/guadi-torino/TP10/master/administrador.PNG'
               }
             />
+            <div className="btn">
+              <Link to="/proyectos">
+                <p style={{ textDecoration: 'none' }}>Ver proyecto</p>
+              </Link>
+            </div>
           </div>
         </div>
         <br />
@@ -41,6 +51,11 @@ const Proyectos = () => {
                 'https://stackblitz.com/files/react-phgqov/github/guadi-torino/TP10/master/todolist.PNG'
               }
             />
+            <div className="btn">
+              <Link to="/proyectos">
+                <p style={{ textDecoration: 'none' }}>Ver proyecto</p>
+              </Link>
+            </div>
           </div>
         </div>
       </center>
